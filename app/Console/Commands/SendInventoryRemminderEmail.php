721c733e393a9233fab1c49f925bfa5790942b69
seeejaay <carljustinem984@gmail.com>
2025-12-23 09:45:11 +0800

    Add inventory reminder email functionality with scheduled command

---
+++ app/Console/Commands/SendInventoryRemminderEmail.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Mail\InventoryReminderMail;
+use Illuminate\Support\Facades\Mail;
+
+class SendInventoryRemminderEmail extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'app:send-inventory-remminder-email';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        //
+        $recipient = "carljustinem984@gmail.com";
+
+        Mail::to($recipient)->send(new InventoryReminderMail());
+
+        $this->info('Inventory reminder email sent successfully to ' . $recipient);
+    }
+}
---
+++ resources/views/emails/inventory_reminder.blade.php
@@ -0,0 +1,2 @@
+{{-- filepath: resources/views/emails/inventory_reminder.blade.php --}}
+<p>Please remember to post your inventory today.</p>
